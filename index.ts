79b7bacfca2cae36c864c5f28f5d0c1b72ae99a5
Tyler Long <tyler4long@gmail.com>
2016-09-21 10:18:23 +0800

    Finish swagger.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-import { segments, routes } from './swagger';
+import { segments, routes, hasIds } from './swagger';
 
 
 console.log(segments);
 console.log('========');
 console.log(routes);
+console.log('========');
+console.log(hasIds);
+
--- swagger.ts
+++ swagger.ts
@@ -1,18 +1,17 @@
 import * as fs from 'fs';
 
 
+// read swagger definition
 const swagger: any = JSON.parse(fs.readFileSync('swagger.json', 'utf-8'));
 
+// all the paths defined in swagger
 const paths: string[] = Object.keys(swagger.paths);
 
 // tokenize the paths above
 const tokenss: string[][] = paths.map(path => path.split('/').filter(token => /^[a-z-]+$/i.test(token)));
 
-// flatten the array above
-const tokens: string[] = [].concat.apply([], tokenss);
-
-// remove duplicates
-const segments = new Set<string>(tokens);
+// flatten the array above and remove duplicates
+const segments = new Set<string>([].concat.apply([], tokenss));
 
 // each segment and their children
 const routes = new Map<string, Set<string>>();
@@ -25,4 +24,13 @@ for (const tokens of tokenss) {
   }
 }
 
-export { segments, routes };
+// whether a segment could have an ID
+const hasIds = new Map<string, boolean>();
+const pathsStr = paths.join('\n');
+for (const segment of segments) {
+  const hasIdRegex = new RegExp(`/${segment}/(?:\{[^{}/]+\}|v1\.0)(?:/|$)`, 'm');
+  hasIds.set(segment, hasIdRegex.test(pathsStr));
+}
+
+
+export { segments, routes, hasIds };
