17d427809cac37c43ed351230f36cb89586c4c3b
Tyler Long <tyler4long@gmail.com>
2016-09-28 10:45:42 +0800

    Add common/util.ts

---
+++ common/util.ts
@@ -0,0 +1,26 @@
+import * as _ from 'lodash';
+
+
+// format code, indent code properly
+const format_code = (str: string): string => {
+  let indent = 0;
+  let result = '';
+  for (const line of str.replace(/\s+$/mg, '').replace(/\n{2,}/g, '\n').split('\n').map(item => item.trim())) {
+    if (line == '}') {
+      indent -= 4;
+    }
+    result += _.repeat(' ', indent) + line + '\n';
+    if (line.endsWith('{')) {
+      indent += 4;
+    }
+  }
+  return result;
+}
+
+// convert string to PascalCase
+const PascalCase = (str: string): string => {
+  return _.upperFirst(_.camelCase(str));
+}
+
+
+export { format_code, PascalCase };
