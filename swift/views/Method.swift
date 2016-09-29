d0a3ec237f4a4273362213a4980a9e97adb35978
Tyler Long <tyler4long@gmail.com>
2016-09-29 10:41:44 +0800

    First working version of http methods

--- swift/views/Method.swift
+++ swift/views/Method.swift
@@ -1,14 +1,18 @@
 // {{ method.description }}
 {% if method.responseName == '' %}
-open func {{ method.method }}() {
-
+open func {{ method.method }}(callback: @escaping (_ error: HTTPError?) -> Void) {
+    rc.{{method.method}}String(self.endpoint()) { string, error in
+      callback(error)
+    }
 }
 {% elif method.responseName == '@Binary' %}
 open func {{ method.method }}() -> NSData {
     return NSData()
 }
 {% else %}
-open func {{ method.method }}() -> {{ method.responseName }} {
-   return {{ method.responseName }}(JSONString: "")!
+open func {{ method.method }}(callback: @escaping (_ t: {{ method.responseName }}?, _ error: HTTPError?) -> Void) {
+   rc.{{method.method}}(self.endpoint()) { (t: {{ method.responseName }}?, error) in
+      callback(t, error)
+   }
 }
 {% endif %}
