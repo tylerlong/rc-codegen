16c456a9bd858ed52a8181365ecd426f21a5b70d
Tyler Long <tyler4long@gmail.com>
2016-11-14 09:47:07 +0800

    Start generating C#

--- csharp/index.ts
+++ csharp/index.ts
@@ -1,5 +1,43 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as nunjucks from 'nunjucks';
+import * as _ from 'lodash';
+import { swagger, segments, actions, segmentIds, children } from '../common/swagger';
+import { format_code, PascalCase } from '../common/util';
+
+
+// template engine
+const engine = nunjucks.configure(path.join(__dirname, 'views'), {
+  autoescape: false,
+  trimBlocks: true,
+  lstripBlocks: true,
+});
+const http_method = (str: string): string => {
+  if (str == 'list') {
+    return 'get';
+  }
+  if (_.includes(['get', 'post', 'put', 'delete'], str)) {
+    return str;
+  }
+  throw new RangeError(`Unknown http method: "${str}"`);
+}
+engine.addFilter('http_method', http_method);
+
+
+const render_definitions = (output: string) => {
+  console.log(`render_definitions to ${output}`);
+}
+
+
+const render_paths = (output: string) => {
+  console.log(`render_paths to ${output}`);
+}
+
+
+// the only method to export
 const generate = (output: string) => {
-  console.log(`Generate C# code to ${output}`);
+  render_definitions(output);
+  render_paths(output);
 }
 
 
